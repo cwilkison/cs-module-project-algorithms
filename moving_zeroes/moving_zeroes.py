053ffc8447a6b6525559772caea93207f0624cdc
Cole Wilkison <wilkison.cole@gmail.com>
2020-06-22 08:51:21 -0400

    part 2 mvp

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,12 +2,20 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+# def moving_zeroes(arr):
+#     # Your code here
+#     for i in arr:
+#         if i == 0:
+#             arr.append(arr.pop(arr.index(0)))
+#     return arr
+
 def moving_zeroes(arr):
-    # Your code here
-    for i in arr:
-        if i == 0:
-            arr.append(arr.pop(arr.index(0)))
-    return arr
+	count = arr.count(0)
+	for i in range(0, count):
+		arr.remove(0)
+		arr.append(0)
+	return arr
+
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
